04baeecb044605cf76d58d665724014b19b24e6c
Anna Nazarenko <anna.nazarenko.dev@gmail.com>
2026-03-10 19:02:39 +0100

    Initial Commit

---
+++ Naptime/NaptimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaptimeApp.swift
+//  Naptime
+//
+//  Created by Anna Nazarenko on 10/3/26.
+//
+
+import SwiftUI
+
+@main
+struct NaptimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
